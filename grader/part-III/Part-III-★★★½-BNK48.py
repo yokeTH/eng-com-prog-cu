b57f929e683bb7e9fb2889e9f144adf192ec5f7f
yokeTH <sern.dev@gmail.com>
2022-11-10 15:06:17 +0700

    1011221506

---
+++ grader/part-III/Part-III-★★★½-BNK48.py
@@ -0,0 +1,44 @@
+members = {}
+otas = {}
+ln = input()
+while len(ln) > 1:
+    ota, name, score = ln.split()
+    if name in members:
+        members[name]['otas'].add(ota)
+        members[name]['score'] += int(score)
+    else:
+        members[name] = {'otas':{ota},'score':int(score),'kami':set()}
+    if ota in otas:
+        if otas[ota]['kami'] == name:
+            otas[ota]['score'] += int(score)
+        elif otas[ota]['score'] == int(score) and otas[ota]['kami'] > name:
+            otas[ota]['kami'] = name
+        elif otas[ota]['score'] < int(score):
+            otas[ota]['score'] = int(score)
+            otas[ota]['kami'] = name
+    else:
+        otas[ota] = {}
+        otas[ota]['score'] = int(score)
+        otas[ota]['kami'] = name
+    ln = input()
+
+for k in otas:
+    members[otas[k]['kami']]['kami'].add(k)
+
+rawAns = []
+
+if ln == '1':
+    for k in members:
+        rawAns.append([-members[k]['score'],k])
+elif ln == '2':
+    for k in members:
+        rawAns.append([-len(members[k]['otas']),k])
+elif ln == '3':
+    for k in members:
+        rawAns.append([-len(members[k]['kami']),k])
+
+rawAns.sort()
+ans = []
+for e in rawAns[:3]:
+    ans.append(e[1])
+print(*ans,sep=', ')
