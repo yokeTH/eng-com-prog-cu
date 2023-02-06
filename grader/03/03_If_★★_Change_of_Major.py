32787d533f8060d9e1e18e684ce94b5308ecbc02
yokeTH <sern.dev@gmail.com>
2023-02-06 10:03:52 +0700

    fix

--- grader/03/03_If_★★_Change_of_Major.py
+++ grader/03/03_If_★★_Change_of_Major.py
@@ -1,13 +1,23 @@
-grade = {'A':4, 'B':3, 'C':2, 'D':1}
+A = input().split()
+B = input().split()
 
-student1 = input().split()
-student2 = input().split()
-
-if student1[2] != 'A' and student2[2] != 'A':
+if (A[2] != 'A' and A[3] < 'D' and A[4] < 'D') or (B[2] != 'A' and B[3] < 'D' and B[4] < 'D'):
     print(None)
-elif student1[2] != 'A':
-    print('2A')
-elif student2[2] != 'A':
-    print('1A')
+elif A[2] != 'A':
+    print(B[0])
+elif B[2] != 'A':
+    print(A[0])
+elif float(A[1]) > float(B[1]):
+    print(A[0])
+elif float(A[1]) < float(B[1]):
+    print(B[0])
+elif A[3] < B[3]:
+    print(A[0])
+elif A[3] > B[3]:
+    print(B[0])
+elif A[4] < B[4]:
+    print(A[0])
+elif A[4] > B[4]:
+    print(B[0])
 else:
     print('BOTH')
